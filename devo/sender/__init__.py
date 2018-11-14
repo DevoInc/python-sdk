b31484e2aded634dfb0f228214ccf5d76e170d27
Ovidiu Mircea Moldovan <ovidiu.moldovan@devo.com>
2018-11-14 10:55:30 +0100

    Added: Wrapper for Sender class
     * Now it can be used throught settings files e.g. django
     * SSL and TCP use cases covered
     * Test cases updated

--- devo/sender/__init__.py
+++ devo/sender/__init__.py
@@ -1,4 +1,4 @@
 # coding=utf-8
-from .data import Sender, SenderConfigTCP, SenderConfigSSL, DevoSenderException
+from .data import Sender, SenderConfigTCP, SenderConfigSSL, DevoSenderException, SenderHandler
 from .transformsyslog import *
 from .lookup import Lookup
