757fc35ff162d833a93a9fe8b53b07333826c22a
José Ramón Afonso <joseramon.afonso@devo.com>
2024-10-02 13:23:29 +0200

    Fix[AWS Jobs]: Fixed destination jobs in AWS/Serrea

--- devo/__version__.py
+++ devo/__version__.py
@@ -1,6 +1,6 @@
 __description__ = "Devo Python Library."
 __url__ = "http://www.devo.com"
-__version__ = "5.4.1"
+__version__ = "5.4.2"
 __author__ = "Devo"
 __author_email__ = "support@devo.com"
 __license__ = "MIT"
